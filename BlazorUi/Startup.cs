493bec3af26f548aa0ff6f64da07b5fced9bf8bb
Dan Hilderbrand <16111574+danh955@users.noreply.github.com>
2020-12-03 16:02:32 +0900

    Added BlazorUi project     Removed counter     Added GameEngine project     New layout     Added Stylecop and FxCop     Added Blazorise.Bulma     Added PlazorUi

---
+++ BlazorUi/Startup.cs
@@ -0,0 +1,88 @@
+// <copyright file="Startup.cs" company="None">
+// Free and open source code.
+// </copyright>
+
+namespace BlazorUi
+{
+    using Blazorise;
+    using Blazorise.Bulma;
+    using Blazorise.Icons.FontAwesome;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
+    /// <summary>
+    /// Startup class.
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <param name="configuration">IConfiguration.</param>
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        /// <summary>
+        /// Gets configuration.
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940 .
+        /// </summary>
+        /// <param name="services">IServiceCollection.</param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+              .AddBlazorise(options =>
+              {
+                  options.ChangeTextOnKeyPress = true;
+              })
+              .AddBulmaProviders()
+              .AddFontAwesomeIcons();
+
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">IApplicationBuilder.</param>
+        /// <param name="env">IWebHostEnvironment.</param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                //// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app?.ApplicationServices
+              .UseBulmaProviders()
+              .UseFontAwesomeIcons();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
